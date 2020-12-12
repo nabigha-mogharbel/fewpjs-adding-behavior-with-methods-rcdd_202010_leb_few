2fb9185dd46e98a3f25f8a504ba2410c2b8f2abd
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-12 11:11:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+class Cat{
+  constructor(name,sex){
+    this.name=name
+    this.sex=sex
+    function speak(){return `${this.name} says meow!`}
+  }
+}
+class Dog{
+    constructor(name,sex){
+    this.name=name
+    this.sex=sex
+    function speak(){return `${this.name} says woof!`}
+}
+class Bird{
+    constructor(name,sex){
+    this.name=name
+    this.sex=sex
+    function speak(){
+     this.sex==="male"?  `It's me! ${this.name},the parrot!`:`${this.name} says squawk!`}
+}
